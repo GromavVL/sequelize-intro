5e8f219b0f1245148dfb088297e2eea0987777f1
Vlad <vlad086367@gmail.com>
2026-03-30 14:23:12 +0300

    Add Associations

--- models/student.js
+++ models/student.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      // define association here
+      Student.belongsTo(models.Group, { foreignKey: 'groupId' });
     }
   }
   /* Обмеження
